5bea33abf0a7291ce275907794abfd18b7e9ce3e
MuadDib3 <myaddib3@gmail.com>
2023-02-21 22:04:45 +0300

    ok

--- js/script.js
+++ js/script.js
@@ -10,6 +10,9 @@ function toggleNav () {
    document.documentElement.classList.toggle("no-scroll");
 }
 
+// css style gap from .content
+const contentGap = Number (window.getComputedStyle(document.querySelector('.content')).gap.slice(0, -2));
+
 //scroll to section from nav
 
 const navLinks = document.querySelectorAll('.links__item[data-goto]');
@@ -27,14 +30,14 @@ if (navLinks.length > 0) {
             const navModal = document.getElementById('modal-nav');
             navModal.classList.remove('modal-nav_active');
             document.documentElement.classList.remove("no-scroll");
-            const gotoBlockValue = gotoBlock.getBoundingClientRect().top - 20 + scrollY - document.querySelector('.nav__button').offsetHeight;
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('.nav__button').offsetHeight;
             window.scrollTo({
                top: gotoBlockValue,
                behavior: "smooth",
             });
          }
          else {
-            const gotoBlockValue = gotoBlock.getBoundingClientRect().top - 35 + scrollY;
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY;
             window.scrollTo({
                top: gotoBlockValue,
                behavior: "smooth",
@@ -52,19 +55,34 @@ window.addEventListener('scroll', () => {
 
 	document.querySelectorAll('section').forEach((el, i) => {
       if (document.documentElement.clientWidth >= 650) {
-         if (el.offsetTop + document.querySelector('h3').clientHeight - document.documentElement.clientHeight <= scrollDistance) {
 
+         if (el.offsetTop - contentGap <= scrollDistance) {
             document.querySelectorAll('.nav a').forEach((el) => {
                if (el.classList.contains('links__item_active')) {
                   el.classList.remove('links__item_active');
                }
             });
+            document.querySelectorAll('.nav li')[i].querySelector('a').classList.add('links__item_active');
+         }
+         //fix last link
+         let pageHeight = Math.max(
+            document.body.scrollHeight, document.documentElement.scrollHeight,
+            document.body.offsetHeight, document.documentElement.offsetHeight,
+            document.body.clientHeight, document.documentElement.clientHeight
+          );
 
-         document.querySelectorAll('.nav li')[i].querySelector('a').classList.add('links__item_active');
+         if (pageHeight <= scrollDistance + document.documentElement.clientHeight + 1) {
+            document.querySelectorAll('.nav a').forEach((el) => {
+               if (el.classList.contains('links__item_active')) {
+                  el.classList.remove('links__item_active');
+               }
+            });
+            document.querySelectorAll('.nav li')[4].querySelector('a').classList.add('links__item_active');
          }
       }
 	});
 });
+
 
 //slider with Swiper
 
